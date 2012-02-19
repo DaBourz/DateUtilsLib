4f65ed17bd7517ff7a4d9e6dc317aed4b4d4b237
Stéphane Bourzeix <stephane@bourzeix.com>
2012-02-19 15:34:25 +0100

    Added spanish

--- DateUtils.php
+++ DateUtils.php
@@ -40,10 +40,10 @@ class DateUtils {
 	 * $this->load->library('dateutils');
 	 *
 	 * echo $this->dateutils->datefr()					=	27/04/1980
-	 * echo $this->dateutils->datefr(325689845,C,L,n);	=	Dim 27 Avril 1980
-	 * echo $this->dateutils->datefr(325689845,n,c,n);	=	27 avr 1980
-	 * echo $this->dateutils->datefr(325689845,l,n,l);	=	dimanche 27/04/1980 7 heures 35
-	 * echo $this->dateutils->datefr(325689845,h,h,l);	=	7 heures 35
+	 * echo $this->dateutils->datefr(325689845,'C','L','n');	=	Dim 27 Avril 1980
+	 * echo $this->dateutils->datefr(325689845,'n','c','n');	=	27 avr 1980
+	 * echo $this->dateutils->datefr(325689845,'l','n','l');	=	dimanche 27/04/1980 7 heures 35
+	 * echo $this->dateutils->datefr(325689845,'h','h','l');	=	7 heures 35
 	 */
 	function datefr($timestamp = null, $modejour = 'n', $modemois = 'n', $modeheure = 'n')
 	{
@@ -165,6 +165,158 @@ class DateUtils {
     	}
 		
     	 /* Valeur retournée */
+    	return $jour.$mois.$heure;
+	}		
+
+
+	/**
+	 * Format and return the date in spanish
+	 *
+	 * @access	public
+	 * @param	timestamp	unix timestamp
+	 * @param	string		day display formatting
+	 * @param	string		month display formatting
+	 * @param	string		hour display formatting
+	 * @return	string	
+	 *
+	 * Usage:
+	 * Function has 4 params:
+	 * - timestamp: date/time (UNIX format)
+	 * - modejour: l:long (lunes 23) ; c:short (lun 23) ; n:day number (23) ; h:nothing
+	 *   L or C in Caps puts the first letter of the day in Caps (ex C : Lun)
+	 * - modemois: l:long (septiembre) ; c:short (sept) ; n:number (09) ; h:nothing
+	 *   L or C in Caps puts the first letter of the day in Caps (ex C : Lun)
+	 * - modeheure: l:long (7 y 35) ; c:short (7h35) ; n:nothing
+	 *
+	 * Examples :
+	 * $this->load->library('dateutils');
+	 *
+	 * echo $this->dateutils->datees()					=	27/04/1980
+	 * echo $this->dateutils->datees(325689845,'C','L','n');	=	Dim 27 Abril 1980
+	 * echo $this->dateutils->datees(325689845,'n','c','n');	=	27 abr 1980
+	 * echo $this->dateutils->datees(325689845,'l','n','l');	=	domingo 27/04/1980 7 y 35
+	 * echo $this->dateutils->datees(325689845,'h','h','l');	=	7 y 35
+	 */
+	function datees($timestamp = null, $modejour = 'n', $modemois = 'n', $modeheure = 'n')
+	{
+		if ($timestamp==null){$timestamp = $this->defaulttime;}
+		
+    	 /* Nos formatear nuestra cita. */
+    	$jour=date( "d",$timestamp);      // dia
+    	$mois=date( "m",$timestamp);      // mes
+    	$annee=date( "Y",$timestamp);      // ano
+    	$num_jour=date( "w",$timestamp);     // número de días de la semana
+    	$hour=date( "H",$timestamp);         // hora
+    	$minute=date( "i",$timestamp);     // minuto
+		
+    	 /* inicialización*/
+    	$heure= '';
+    	$aff_heure= ' y ';
+    	if ($hour==0 || $hour==1)
+    	    $aff_heure= ' y ';
+		
+    	 /* Se define el número de meses */
+		$zero  =  substr($mois,0,1);
+		if  ($zero  ==   "0")  {
+			$num_mois  =  substr($mois,1,1);  // Se elimina el primer cero
+		} else { 
+    	    $num_mois = $mois ; 
+		}
+		
+    	 /* Nombre largo del día */
+    	$noml_jour[0]  =   "domingo";
+    	$noml_jour[1]  =   "lunes";
+    	$noml_jour[2]  =   "martes";
+    	$noml_jour[3]  =   "miércoles";
+    	$noml_jour[4]  =   "jueves";
+    	$noml_jour[5]  =   "viernes";
+    	$noml_jour[6]  =   "sábado";
+    	 /* Nombre abreviado del día */
+    	$nomc_jour[0]  =   "dom";
+    	$nomc_jour[1]  =   "lun";
+    	$nomc_jour[2]  =   "mar";
+    	$nomc_jour[3]  =   "mié";
+    	$nomc_jour[4]  =   "jue";
+    	$nomc_jour[5]  =   "vie";
+    	$nomc_jour[6]  =   "sáb";
+		
+    	 /* Nombre largo del mes */
+    	$noml_mois[1]    =   "enero";
+    	$noml_mois[2]    =   "febrero";
+    	$noml_mois[3]    =   "marzo";
+    	$noml_mois[4]    =   "abril";
+    	$noml_mois[5]    =   "mayo";
+    	$noml_mois[6]    =   "junio";
+    	$noml_mois[7]    =   "julio";
+    	$noml_mois[8]    =   "agosto";
+    	$noml_mois[9]    =   "septiembre";
+    	$noml_mois[10]   =   "octubre";
+    	$noml_mois[11]   =   "noviembre";
+    	$noml_mois[12]   =   "diciembre";
+    	 /* Nombre abreviado del mes */
+    	$nomc_mois[1]    =   "ene";
+    	$nomc_mois[2]    =   "feb";
+    	$nomc_mois[3]    =   "mar";
+    	$nomc_mois[4]    =   "abr";
+    	$nomc_mois[5]    =   "may";
+    	$nomc_mois[6]    =   "jun";
+    	$nomc_mois[7]    =   "jul";
+    	$nomc_mois[8]    =   "ago";
+    	$nomc_mois[9]    =   "sep";
+    	$nomc_mois[10]   =   "oct";
+    	$nomc_mois[11]   =   "nov";
+    	$nomc_mois[12]   =   "dic";
+		
+    	 /* Vista de Día */
+    	switch ($modejour) {
+    	    case  "l":
+    	    $jour = $noml_jour[$num_jour]. ' '.$jour;                 // sábado 23
+    	    break;
+    	    case  "L":
+    	    $jour = ucfirst($noml_jour[$num_jour]). ' '.$jour;         // Sábado 23
+    	    break;
+    	    case  "c":
+    	    $jour = $nomc_jour[$num_jour]. ' '.$jour;                 // sáb 23
+    	    break;
+    	    case  "C":
+    	    $jour = ucfirst($nomc_jour[$num_jour]). ' '.$jour;         // Sáb 23
+    	    break;
+    	    case  "h":
+    	    $jour =  '';
+    	    break;
+    	}                                                             // default : 23
+    	 /* Vista del Mes */
+    	switch ($modemois) {
+    	    case  "l":
+    	    $mois =  ' '.$noml_mois[$num_mois]. ' '.$annee;              // enero 1999
+    	    break;
+    	    case  "L":
+    	    $mois =  ' '.ucfirst($noml_mois[$num_mois]). ' '.$annee;     // Enero 1999
+    	    break;
+    	    case  "c":
+    	    $mois =  ' '.$nomc_mois[$num_mois]. ' '.$annee;              // ene 1999
+    	    break;
+    	    case  "C":
+    	    $mois =  ' '.ucfirst($nomc_mois[$num_mois]). ' '.$annee;     // Ene 1999
+    	    break;
+    	    case  "h":
+    	    $mois =  '';
+    	    break;
+    	    default:
+    	    $mois =  '/'.$mois. '/'.$annee;                             // /08/1999
+    	    break;
+    	}
+    	 /* Visualización de la hora */
+    	switch ($modeheure) {
+    	    case  "l":
+    	    $heure =  ' '.$hour.$aff_heure.$minute;                     // 23 y 54
+    	    break;
+    	    case  "c":
+    	    $heure =  ' '.$hour. 'h'.$minute;                             // 23h54
+    	    break;
+    	}
+		
+    	 /* Valor devuelto */
     	return $jour.$mois.$heure;
 	}		
 
